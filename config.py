6b92d0b4dd9f630b16e754d4c77338988235b514
sujoybarui22243yy6t7t <baruisujoy345@gmail.com>
2024-09-22 23:10:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7448924519:AAFa3gfN4aMgTtsrbJHHg6K--wkAn2TjDCg")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6590914935")
 
 
 
